b282f5b441de5756480161138da42756ca3ce3d5
mrinex <mrinex@mail.ru>
2020-11-05 18:20:34 +0300

    feat: implement animation on click

--- src/js/moveTile.js
+++ src/js/moveTile.js
@@ -5,14 +5,26 @@ const { checkWin } = require('./checkWin');
 
 function moveTile({ target }) {
     const tiles = document.querySelectorAll('.square');
+    const { width } = tiles[0].getBoundingClientRect();
     for (let i = 0; i < tiles.length; i += 1) {
         if (tiles[i].textContent === target.textContent) {
             if (tiles[i + 1] !== undefined && tiles[i + 1].textContent === '') {
                 if ((i + 1) % Math.sqrt(tiles.length) !== 0) {
                     console.log('1');
                     const element = tiles[i];
-                    tiles[i].remove();
-                    tiles[i + 1].after(element);
+                    const emptySquare = tiles[i + 1];
+                    setTimeout(() => {
+                        tiles[i].remove();
+                        tiles[i + 1].after(element);
+
+                        tiles[i + 1].remove();
+                        element.before(emptySquare);
+
+                        emptySquare.removeAttribute('style');
+                        element.removeAttribute('style');
+                    }, 1000);
+                    element.style.left = `${width + 2}px`;
+                    tiles[i + 1].style.left = `-${width + 2}px`;
                     addStep();
                 }
             }
@@ -20,12 +32,19 @@ function moveTile({ target }) {
                 if ((i - 1) % Math.sqrt(tiles.length) !== Math.sqrt(tiles.length) - 1) {
                     console.log('2');
                     const element = tiles[i];
+                    const emptySquare = tiles[i - 1];
                     setTimeout(() => {
                         tiles[i].remove();
                         tiles[i - 1].before(element);
-                        element.style.left = '0';
+
+                        tiles[i - 1].remove();
+                        element.after(emptySquare);
+
+                        emptySquare.removeAttribute('style');
+                        element.removeAttribute('style');
                     }, 1000);
-                    element.style.left = '-82px';
+                    element.style.left = `-${width + 2}px`;
+                    tiles[i - 1].style.left = `${width + 2}px`;
                     addStep();
                 }
             }
@@ -33,27 +52,45 @@ function moveTile({ target }) {
                 console.log('3');
                 const element = tiles[i];
                 const empty = tiles[i + Math.sqrt(tiles.length)];
-                tiles[i].remove();
-                tiles[i + Math.sqrt(tiles.length)].remove();
-                tiles[i + (Math.sqrt(tiles.length) - 1)].after(element);
-                tiles[i + 1].before(empty);
+                setTimeout(() => {
+                    tiles[i].remove();
+                    tiles[i + Math.sqrt(tiles.length)].remove();
+
+                    tiles[i + (Math.sqrt(tiles.length) - 1)].after(element);
+                    tiles[i + 1].before(empty);
+
+                    empty.removeAttribute('style');
+                    element.removeAttribute('style');
+                }, 1000);
+                element.style.top = `${width + 2}px`;
+                empty.style.top = `-${width + 2}px`;
                 addStep();
             }
             if (tiles[i - Math.sqrt(tiles.length)] !== undefined && tiles[i - Math.sqrt(tiles.length)].textContent === '') {
                 console.log('4');
                 const element = tiles[i];
                 const empty = tiles[i - Math.sqrt(tiles.length)];
-                tiles[i].remove();
-                tiles[i - Math.sqrt(tiles.length)].remove();
-                tiles[i - (Math.sqrt(tiles.length) - 1)].before(element);
-                tiles[i - 1].after(empty);
+                setTimeout(() => {
+                    tiles[i].remove();
+                    tiles[i - Math.sqrt(tiles.length)].remove();
+
+                    tiles[i - (Math.sqrt(tiles.length) - 1)].before(element);
+                    tiles[i - 1].after(empty);
+
+                    empty.removeAttribute('style');
+                    element.removeAttribute('style');
+                }, 1000);
+                element.style.top = `-${width + 2}px`;
+                empty.style.top = `${width + 2}px`;
                 addStep();
             }
         }
-        removeDraggableAll();
     }
-    markDraggable();
-    checkWin();
+    setTimeout(() => {
+        removeDraggableAll();
+        markDraggable();
+        checkWin();
+    }, 1000);
 }
 
 module.exports = {
